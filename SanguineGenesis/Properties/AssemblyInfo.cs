98fd89d5e56349103c4a1688022e3f27d668340f
tomkalva <tomkalva@seznam.cz>
2020-01-02 12:38:11 +0100

    Changed assembly name for the project SanguineGenesis.

--- SanguineGenesis/Properties/AssemblyInfo.cs
+++ SanguineGenesis/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Obecné informace o sestavení se řídí přes následující 
 // sadu atributů. Změnou hodnot těchto atributů se upraví informace
 // přidružené k sestavení.
-[assembly: AssemblyTitle("wpfTest")]
+[assembly: AssemblyTitle("SanguineGenesis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("wpfTest")]
+[assembly: AssemblyProduct("SanguineGenesis")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
